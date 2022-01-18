3c389ad722b08ee657220c688cf751efd3a7acc4
Ivan Skordev <maymunskoa@yahoo.com>
2022-01-19 01:51:33 +0200

    add materialize-css and google icons to the project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import { useEffect } from 'react';
+
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+
 import './App.css';
 
 const App = () => {
+  useEffect(() => {
+    // Initialize Materialize JS
+    M.AutoInit();
+  }, []);
   return <div className='App'>My App</div>;
 };
 
