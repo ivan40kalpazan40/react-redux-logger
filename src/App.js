11fda5c48c67c8dc4b055fb11354b911daa1ed4f
Ivan Skordev <maymunskoa@yahoo.com>
2022-01-19 14:00:55 +0200

    logItem component and loading component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { useEffect } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
+
 import './App.css';
 
 const App = () => {
@@ -10,7 +13,14 @@ const App = () => {
     // Initialize Materialize JS
     M.AutoInit();
   }, []);
-  return <div className='App'>My App</div>;
+  return (
+    <>
+      <SearchBar />
+      <div className='container'>
+        <Logs />
+      </div>
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/layout/Preloader.js
@@ -0,0 +1,7 @@
+const Preloader = () => {
+  return <div className="progress blue lighten-4">
+      <div className="indeterminate blue"></div>
+  </div>;
+};
+
+export default Preloader;
