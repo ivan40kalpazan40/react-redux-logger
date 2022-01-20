b3726e645176c4153ea418842b647abddd8e84db
Ivan Skordev <maymunskoa@yahoo.com>
2022-01-20 14:37:05 +0200

    logReducer and logActions

---
+++ src/actions/logActions.js
@@ -0,0 +1,21 @@
+import { GET_LOGS, LOGS_ERROR, SET_LOADING } from './types';
+
+export const getLogs = () => async (dispatch) => {
+  try {
+    setLoading();
+
+    const res = await fetch(`/logs`);
+    const data = await res.json();
+
+    dispatch({ type: GET_LOGS, payload: data });
+  } catch (err) {
+    dispatch({ type: LOGS_ERROR, payload: err.response.data });
+  }
+};
+
+// Set Loading
+export const setLoading = () => {
+  return {
+    type: SET_LOADING,
+  };
+};
